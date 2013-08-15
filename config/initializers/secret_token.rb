d84c100132bfcfd8a8fcd3a7d282ed67df00faa2
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-14 17:19:19 -0700

    add rails 4 framework w/ sqlite3 db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DepotRails::Application.config.secret_key_base = 'ece149ff14278598ff5fbdc6d64565300e45feaf382add0f563037975eb381d061700fe193a6db60b85ac92daa8b579c6e1bcdeac2908f8e62321e817f9ef7b1'
